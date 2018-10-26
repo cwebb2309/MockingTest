594ab3091bf3f5ac8f4748168c5c1aaa5a4e95d1
Chris <chriswebb2309@gmail.com>
2018-10-26 11:33:37 +0100

    now multiple return and inputs

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
 type bankDetail interface {
-	getBalance(accNo string) int
+	getBalance(accNo string, sortCode string) (int, int)
 }
 
 type personalAccount struct {
 }
 
-func (p personalAccount) getBalance(accNo string) int {
+func (p personalAccount) getBalance(accNo string, sortCode string) (int, int) {
 	if accNo == "0001" {
-		return 1200
+		return 1200, 2700
 	}
-	return 1000
+	return 1000, 2200
 }
 
 func main() {
 	var account personalAccount
 
-	bal := queryBalance(account, "0001")
+	bal, available := queryBalance(account, "0001", "01-01-01")
 	println(bal)
+	print(available)
 
 }
 
-func queryBalance(a bankDetail, accNo string) int {
-	return a.getBalance(accNo)
+func queryBalance(a bankDetail, accNo string, sortCode string) (int, int) {
+	bal, avail := a.getBalance(accNo, sortCode)
+	return bal, avail
 }
--- main_test.go
+++ main_test.go
@@ -11,14 +11,16 @@ type balanceMock struct {
 	mock.Mock
 }
 
-func (p balanceMock) getBalance(accNo string) int {
-	args := p.Called(accNo)
-	return args.Int(0)
+func (p balanceMock) getBalance(accNo string, sortCode string) (int, int) {
+	args := p.Called(accNo, sortCode)
+	return args.Int(0), args.Int(1)
 }
 
-func TestMockMethodWithArgs(t *testing.T) {
+func TestQueryBalance(t *testing.T) {
 	theMock := balanceMock{}
-	theMock.On("getBalance", "0000").Return(1000, 0)
-	res := queryBalance(theMock, "0000")
-	assert.Equal(t, 1000, res)
+	theMock.On("getBalance", "0000", "01-01-01").Return(1000, 2200)
+
+	bal, avail := queryBalance(theMock, "0000", "01-01-01")
+	assert.Equal(t, 1000, bal)
+	assert.Equal(t, 2200, avail)
 }
